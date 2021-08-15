f52287d9a4ce2bc49d7f4054f9c5c5f5026bd370
mansouralbatran <mansouralbatran@gmail.com>
2021-08-15 19:08:00 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ class App extends React.Component {
 
     
     });
-    let dataimg =`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&center=${this.state.locationout.lat},${this.state.locationout.lon}&size=600x600&zoom=14&path=fillcolor:%2390EE90|weight:2|color:blue|17.452945,78.380055|17.452765,78.382026|17.452020,78.381375|17.452045,78.380846|17.452945,78.380055`
+    let dataimg =`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&center=${this.state.locationout.lat},${this.state.locationout.lon}`
 
     let dataresult2 = await axios.get(dataimg);
     console.log("rrrrrrrrrrrrr",dataresult2);
@@ -54,6 +54,7 @@ render(){
       <label>loction Name </label>
       <input type='text' name='city' placeholder='locationname' />
       <button type='submit'>submit</button>
+      
 
 
     </form>
@@ -63,6 +64,7 @@ render(){
 
 
     <p>loction </p>
+    {this.state.showData &&<img src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&center=${this.state.locationout.lat},${this.state.locationout.lon}`} alt="" />}
 
 
 
